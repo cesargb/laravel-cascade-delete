2d3fe87eac5bdb23497727965cea2bb5493c8fc7
Cesar <cesargb@gmail.com>
2019-02-27 09:55:40 +0100

    add compatibility testbench 3.8

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,7 +17,7 @@ abstract class TestCase extends Orchestra
     /**
      * Setup the test environment.
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
