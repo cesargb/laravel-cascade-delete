fc4290050b37db1bc72ee852c9d54f9dd2d143c4
Cesar <cesargb@gmail.com>
2019-07-14 12:38:42 +0200

    fix tests deleted batch

--- tests/MorphCleanCommandTest.php
+++ tests/MorphCleanCommandTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use Tests\Models\User;
+use Tests\Models\Image;
 use Tests\Models\Photo;
 use Tests\Models\Video;
 use Illuminate\Support\Facades\DB;
@@ -9,25 +11,66 @@ use Illuminate\Support\Facades\Artisan;
 
 class MorphCleanCommandTest extends TestCase
 {
-    public function test_command()
+    public function test_command_delete_morph_one()
     {
-        $this->assertTrue(true);
+        $totalImages = Image::count();
 
-        $totalOptions = DB::table('options')->count();
-        $totalTags = DB::table('taggables')->count();
+        $deleteImages = 1;
 
-        $deleteOptions = Photo::first()->options()->count();
-        $deleteTags = Video::first()->tags()->count();
+        User::where('id', User::first()->id)->delete();
 
-        Photo::first()->delete();
-        Video::first()->delete();
+        $this->assertEquals(
+            $totalImages,
+            Image::count()
+        );
 
         Artisan::call('morph:clean');
+
+        $this->assertEquals(
+            $totalImages - $deleteImages,
+            Image::count()
+        );
+    }
+
+    public function test_command_delete_morph_many()
+    {
+        $totalOptions = DB::table('options')->count();
+
+        $deleteOptions = Photo::first()->options()->count();
+
+        Photo::where('id', Photo::first()->id)->delete();
+
+        $this->assertEquals(
+            $totalOptions,
+            DB::table('options')->count()
+        );
+
+        Artisan::call('morph:clean');
+
+        $this->assertGreaterThan(0, $deleteOptions);
 
         $this->assertEquals(
             $totalOptions - $deleteOptions,
             DB::table('options')->count()
         );
+    }
+
+    public function test_command_delete_morph_to_manyn()
+    {
+        $totalTags = DB::table('taggables')->count();
+
+        $deleteTags = Video::first()->tags()->count();
+
+        Video::where('id', Video::first()->id)->delete();
+
+        $this->assertEquals(
+            $totalTags,
+            DB::table('taggables')->count()
+        );
+
+        Artisan::call('morph:clean');
+
+        $this->assertGreaterThan(0, $deleteTags);
 
         $this->assertEquals(
             $totalTags - $deleteTags,
