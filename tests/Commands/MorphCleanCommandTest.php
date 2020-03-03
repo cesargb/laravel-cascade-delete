4f86a5c4cd8576287a89d60e532bad360cb8de98
Cesar <cesargb@gmail.com>
2020-03-03 15:21:10 +0100

    tests verfy l5.5

--- tests/Commands/MorphCleanCommandTest.php
+++ tests/Commands/MorphCleanCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Commands;
 
+use Illuminate\Support\Facades\App;
 use Tests\Models\Image;
 use Tests\Models\User;
 use Tests\TestCase;
@@ -21,7 +22,13 @@ class MorphCleanCommandTest extends TestCase
         $this->assertEquals(2, Image::count());
         $this->assertNotNull(User::first()->image);
 
-        $this->artisan('morph:clean')->assertExitCode(0);
+        list($versionMayor, $versionMinor) = explode('.', App::version());
+
+        if ($versionMayor == 5 && $versionMinor == 5) {
+            $this->artisan('morph:clean');
+        } else {
+            $this->artisan('morph:clean')->assertExitCode(0);
+        }
 
         $this->assertEquals(1, Image::count());
         $this->assertNotNull(User::where('id', 2)->first()->image);
